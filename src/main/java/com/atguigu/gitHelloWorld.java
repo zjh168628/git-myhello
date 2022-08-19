431b3a218b16872ce4d974587d77822f0b81e748
zjh168628 <108135582+zjh168628@users.noreply.github.com>
2022-08-19 20:05:10 +0800

    git hello02

--- src/main/java/com/atguigu/gitHelloWorld.java
+++ src/main/java/com/atguigu/gitHelloWorld.java
@@ -3,5 +3,6 @@ package com.atguigu;
 public class gitHelloWorld {
     public static void main(String[] args) {
         System.out.println("大家好我是练习时长两年半的个人练习生");
+        System.out.pringln("鸡你太美")
     }
 }
